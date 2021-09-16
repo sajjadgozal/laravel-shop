22550c5fd9b213accfc4c7e3a3dc14a0fa8ddbda
Sajjad Gozalzadeh <sajjad.gozalzadeh@paribu.com>
2021-09-17 01:13:03 +0300

    api namespace bug fix

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Inertia\Inertia;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
